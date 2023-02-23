c830c7436f9cf6aeddfc7fc3fb0c49562b4d7ad3
SakshamBamotra <120676541+SakshamBamotra@users.noreply.github.com>
2023-02-23 20:07:00 +0530

    Add files via upload

---
+++ EVEN_NO_.C
@@ -0,0 +1,12 @@
+       #include<stdio.h>
+       #include<conio.h>
+       void main()           //even no's upto 20.
+       {
+       clrscr();
+       int i;
+       for(i=2;i<=20;i=i+2)
+       {
+       printf("\nthe even no's is:%d",i);
+       }
+	    getch();
+	    }
